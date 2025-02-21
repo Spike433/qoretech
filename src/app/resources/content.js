d96e13e8c1c599b4a3afefc6583bb3d6b613b383
Spike433 <juhacar116@gmail.com>
2025-02-21 23:55:59 +0100

    Update person details and social link

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,15 +1,15 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Mateo ",
-  lastName: "Elez",
+  firstName: "QoreTech",
+  lastName: "",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "Europe/Zagreb", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const social = [
@@ -23,7 +23,7 @@ const social = [
   {
     name: "Email",
     icon: "email",
-    link: "mailto:example@gmail.com",
+    link: "mailto:mateo@qoretech.eu",
   },
 ];
 
